20a6707287a984546c928381fdf7dccf3d1ca08f
Huseyin <huseyin.alp@std.ieu.edu.tr>
2024-04-11 09:08:22 +0300

    A very basic website
    This is my first project about creating website. If you want to use you can change the sentences.

---
+++ a basic website using streamlit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title='Website' , page_icon=':tada:', layout="wide")
+
+st.subheader('Hello Everyone :wave:')
+st.title('a Developer from Turkiye')
+st.write('a Guy who starts coding new and tries to develop her country up to other developed countries ')
+st.write('[For the other things >](https://github.com/ColonelMillerr)')
+
